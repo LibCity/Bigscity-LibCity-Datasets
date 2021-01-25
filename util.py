903b2c2171d469318d47cf23541d99362fd5fd00
jjw <jwjiang_buaa@163.com>
2021-01-25 15:47:07 +0800

    util

---
+++ util.py
@@ -0,0 +1,21 @@
+import time
+import os
+
+
+def timestamp_datetime(secs):
+    dt = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime(secs))
+    return dt
+
+
+def datetime_timestamp(dt):
+    s = time.mktime(time.strptime(dt, '%Y-%m-%dT%H:%M:%SZ'))
+    return int(s)
+
+
+def add_TZ(dt):
+    return time.strftime('%Y-%m-%dT%H:%M:%SZ', time.strptime(dt, '%Y-%m-%d %H:%M:%S'))
+
+
+def ensure_dir(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
