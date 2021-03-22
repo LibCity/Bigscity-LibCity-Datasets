921c1ce20e3b9a3a435ea01725bef06ec2521332
冯若怡 <18377347@buaa.edu.cn>
2021-03-22 22:48:19 +0800

    fry

---
+++ nyctaxi20140112.py
@@ -0,0 +1,156 @@
+import h5py
+import pandas as pd
+import numpy as np
+import json
+import util
+
+outputdir = 'output/NYCTAXI20140112'
+util.ensure_dir(outputdir)
+
+dataurl = 'input/NYCTAXI20140112/'
+dataname = outputdir+'/NYCTAXI20140112'
+
+f = h5py.File(dataurl + 'NYC2014.h5', 'r')
+date_df = pd.DataFrame(np.array(f['date']))
+data = np.array(f['data'])
+
+
+def get_Geo():
+    L = []
+    ind = 0
+    for x in range(15):
+        for y in range(5):
+            L.append([ind, "Polygon", "[]", x, y])
+            ind += 1
+    return L
+
+
+def remove_imcomplete_days(data, timestamps, t=48):
+    print("before removing", len(data))
+    date = []
+    days = []
+    days_incomplete = []
+    i = 0
+    print(len(timestamps))
+    while i < len(timestamps):
+        if int(str(timestamps[i])[10:12]) != 1:
+            i += 1
+        elif i + t - 1 < len(timestamps) \
+                and int(str(timestamps[i + t - 1])[10:12]) == t:
+            for j in range(48):
+                date.append(timestamps[i + j])
+            days.append(str(timestamps[i])[2:10])
+            i += t
+        else:
+            days_incomplete.append(str(timestamps[i])[2:10])
+            i += 1
+    print("imcomplete days", days_incomplete)
+    days = set(days)
+    idx = []
+    for i, t in enumerate(timestamps):
+        if str(timestamps[i])[2:10] in days:
+            idx.append(i)
+    data_ = data[idx]
+    print(len(date))
+    print(len(data_))
+    return date, data_
+
+
+def del_date(date):
+    date_str = str(date)
+    s0 = date_str[2:6]
+    s1 = date_str[6:8]
+    s2 = date_str[8:10]
+    s3 = date_str[10:12]
+    num_s3 = int(s3) - 1
+    num = 0
+    if num_s3 % 2 == 0:
+        num = int(num_s3 * 0.5)
+        if num < 10:
+            str_s3 = '0' + str(num)
+        else:
+            str_s3 = str(num)
+        s = s0 + '-' + s1 + '-' + s2 + 'T' + str_s3 + ':00:00' + 'Z'
+    else:
+        num = (num_s3 - 1) * 0.5
+        num = int(num)
+        if num < 10:
+            str_s3 = '0' + str(num)
+        else:
+            str_s3 = str(num)
+        s = s0 + '-' + s1 + '-' + s2 + 'T' + str_s3 + ':30:00' + 'Z'
+
+    return s
+
+
+new_date, new_data = remove_imcomplete_days(np.array(f['data']),
+                                            np.array(f['date']))
+date_df = pd.DataFrame(new_date)
+date_df['time'] = date_df[0].apply(del_date)
+
+
+def get_Dyan():
+    ind = 0
+    L = []
+    for x in range(15):
+        for y in range(5):
+            for time in range(len(date_df['time'])):
+                L.append([ind, "state", date_df['time'][time], x, y,
+                          new_data[time][0][x][y], new_data[time][1][x][y]])
+                ind += 1
+    return L
+
+
+L0 = get_Geo()
+pd.DataFrame(L0, columns=["geo_id", "type", "coordinates", "row_id",
+                          "column_id"]).to_csv(dataname + '.geo', index=None)
+
+L1 = get_Dyan()
+pd.DataFrame(L1, columns=["dyna_id", "type", "time", "row_id",
+                          "column_id", "pickup", "dropoff"])\
+    .to_csv(dataname + '.grid', index=None)
+
+
+ext = h5py.File(dataurl + 'Meteorology.h5', 'r')
+date = np.array(ext['date'])
+Temperature = np.array(ext['Temperature'])
+Weather = np.array(ext['Weather'])
+WindSpeed = np.array(ext['WindSpeed'])
+datenew = []
+
+for da in date:
+    datenew.append(del_date(da))
+ext_id = np.array(range(len(datenew)))
+
+extdf = pd.DataFrame()
+extdf['ext_id'] = ext_id
+extdf['time'] = datenew
+extdf['Temperature'] = Temperature
+extdf['WindSpeed'] = WindSpeed
+columns = ['Temperature', 'WindSpeed']
+for i in range(Weather.shape[1]):
+    extdf['Weather'+str(i)] = Weather[:, i]
+    columns.append('Weather' + str(i))
+extdf.to_csv(dataname + '.ext', index=False)
+
+config = dict()
+config['geo'] = dict()
+config['geo']['including_types'] = ['Polygon']
+config['geo']['Polygon'] = {"row_id": 'num', "column_id": 'num'}
+config['grid'] = dict()
+config['grid']['including_types'] = ['state']
+config['grid']['state'] = {'row_id': 15, 'column_id': 5,
+                           "pickup": 'num', "dropoff": 'num'}
+config['info'] = dict()
+config['info']['data_col'] = ['pickup', 'dropoff']
+config['info']['ext_col'] = columns
+config['info']['data_files'] = ['NYCTAXI20140112']
+config['info']['geo_file'] = 'NYCTAXI20140112'
+config['info']['ext_file'] = 'NYCTAXI20140112'
+config['info']['output_dim'] = 2
+config['info']['init_weight_inf_or_zero'] = 'inf'
+config['info']['set_weight_link_or_dist'] = 'dist'
+config['info']['calculate_weight_adj'] = False
+config['info']['weight_adj_epsilon'] = 0.1
+json.dump(config, open(outputdir + '/config.json', 'w',
+                       encoding='utf-8'), ensure_ascii=False)
---
+++ nyctaxi20160102.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+import json
+import util
+
+outputdir = 'output/NYCTAXI20160102'
+util.ensure_dir(outputdir)
+
+dataurl = 'input/NYCTAXI20160102/'
+dataname = outputdir+'/NYCTAXI20160102'
+
+data_train = np.load(dataurl + 'taxi_train.npz')
+data_train_flow = data_train['flow']
+data_test = np.load(dataurl + 'taxi_test.npz')
+data_test_flow = data_test['flow']
+
+
+def get_Geo():
+    L = []
+    ind = 0
+    for x in range(16):
+        for y in range(12):
+            L.append([ind, "Polygon", "[]", x, y])
+            ind += 1
+    return L
+
+
+def get_time(x):
+    if x % 2 == 0:
+        x = int(x*0.5)
+        y = int((x - x % 24) / 24 + 1)
+        if y > 31:
+            month = 2
+            day = y - 31
+            if day < 10:
+                day_str = '0' + str(day)
+            else:
+                day_str = str(day)
+        else:
+            month = 1
+            day = y
+            if day < 10:
+                day_str = '0' + str(day)
+            else:
+                day_str = str(day)
+        hour = x % 24
+        if hour < 10:
+            hour_str = '0' + str(hour)
+        else:
+            hour_str = str(hour)
+
+        return '2016-0' + str(month) + '-' + day_str + \
+               'T' + hour_str + ':00:00Z'
+    else:
+        x = x - 1
+        x = int(x * 0.5)
+        y = int((x - x % 24) / 24 + 1)
+        if y > 31:
+            month = 2
+            day = y - 31
+            if day < 10:
+                day_str = '0' + str(day)
+            else:
+                day_str = str(day)
+        else:
+            month = 1
+            day = y
+            if day < 10:
+                day_str = '0' + str(day)
+            else:
+                day_str = str(day)
+        hour = x % 24
+        if hour < 10:
+            hour_str = '0' + str(hour)
+        else:
+            hour_str = str(hour)
+
+        return '2016-0' + str(month) + '-' + day_str + \
+               'T' + hour_str + ':30:00Z'
+
+
+def get_Dyan():
+    ind = 0
+    L = []
+    for x in range(16):
+        for y in range(12):
+            for time in range(1920):
+                L.append([ind, "state", get_time(time), x, y,
+                          data_train_flow[time][x][y][0],
+                          data_train_flow[time][x][y][1]])
+                ind += 1
+            for time in range(960):
+                L.append([ind, "state", get_time(time+1920), x, y,
+                          data_test_flow[time][x][y][0],
+                          data_test_flow[time][x][y][1]])
+                ind += 1
+    return L
+
+
+L0 = get_Geo()
+pd.DataFrame(L0, columns=["geo_id", "type", "coordinates", "row_id",
+                          "column_id"]).to_csv(dataname + '.geo', index=None)
+L1 = get_Dyan()
+pd.DataFrame(L1, columns=["dyna_id", "type", "time", "row_id",
+                          "column_id", "inflow", "outflow"])\
+    .to_csv(dataname + '.grid', index=None)
+
+
+config = dict()
+config['geo'] = dict()
+config['geo']['including_types'] = ['Polygon']
+config['geo']['Polygon'] = {"row_id": 'num', "column_id": 'num'}
+config['grid'] = dict()
+config['grid']['including_types'] = ['state']
+config['grid']['state'] = {'row_id': 16,
+                           'column_id': 12, 'inflow': 'num',
+                           'outflow': 'num'}
+config['info'] = dict()
+config['info']['data_col'] = ['inflow', 'outflow']
+config['info']['data_files'] = ['NYCTAXI20160102']
+config['info']['geo_file'] = 'NYCTAXI20160102'
+config['info']['output_dim'] = 2
+config['info']['init_weight_inf_or_zero'] = 'inf'
+config['info']['set_weight_link_or_dist'] = 'dist'
+config['info']['calculate_weight_adj'] = False
+config['info']['weight_adj_epsilon'] = 0.1
+json.dump(config, open(outputdir + '/config.json', 'w',
+                       encoding='utf-8'), ensure_ascii=False)
