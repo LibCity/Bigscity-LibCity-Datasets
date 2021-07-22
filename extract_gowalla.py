7dd54176ce57b4943354786f23f0998e25c7a181
Melles <842595644@qq.com>
2021-07-22 08:24:41 +0800

    add trajectory script

---
+++ extract_gowalla.py
@@ -0,0 +1,65 @@
+import pandas as pd
+
+traj = pd.read_csv('./Gowalla.txt', sep='\t', names=['user', 'check_in_time', 'latitude', 'longitude', 'location_id'])
+
+# 不太确当数据筛选是否应该在这一步做
+# filter_num = 20
+# traj_group_user = traj.groupby('user').count()
+# filter_user = traj_group_user[traj_group_user['check_in_time'] > filter_num]
+# user_index = filter_user.index.tolist()
+# traj_group_location = traj.groupby('location_id').count()
+# filter_location = traj_group_location[traj_group_location['check_in_time'] > 10]
+# location_index = filter_location.index.tolist()
+# filter_traj = traj[traj['user'].isin(user_index) & traj['location_id'].isin(location_index)]
+# 因为他没有单独的 usr 和 loc profile 因此需要先从 traj 中抽出 usr 和 poi 信息
+
+poi_info = traj.filter(items=['latitude', 'longitude', 'location_id'])
+# 有些 poi 的经纬度有2个，感觉是定位定飘了，相差在百米范围内，因此取 mean
+poi_info = poi_info.groupby('location_id').mean()
+poi_info.reset_index(inplace=True)
+poi_info['type'] = 'Point'
+
+# 计算 coordinates
+coordinates = []
+for index, row in poi_info.iterrows():
+    coordinates.append('[{},{}]'.format(row['longitude'], row['latitude']))
+
+poi_info['coordinates'] = coordinates
+poi_info = poi_info.drop(['latitude', 'longitude'], axis=1)
+poi_info = poi_info.rename(columns={'location_id': 'geo_id'})
+poi_info.to_csv('./gowalla.geo', index=False)
+
+# 处理 usr
+usr_info = pd.unique(traj['user'])
+usr_info = pd.DataFrame(usr_info, columns=['usr_id'])
+# 因为没有 properties 信息所以直接存吧
+usr_info.to_csv('./gowalla.usr', index=False)
+
+# 但是有 rel
+rel = pd.read_csv('./Gowalla_edges.txt', sep='\t', names=['origin_id', 'destination_id'])
+# rel 表中包含了一些没有出现在 check_in 里面的 usr，删去之
+b = usr_info['usr_id'].tolist()
+rel = rel[(rel['origin_id'].isin(b)) & (rel['destination_id'].isin(b))] 
+rel['rel_id'] = rel.index
+rel['type'] = 'usr'
+rel = rel.reindex(columns=['rel_id', 'type', 'origin_id', 'destination_id'])
+rel.to_csv('./gowalla.rel', index=False)
+
+# 处理 traj
+dyna = traj.drop(['latitude', 'longitude'], axis=1)
+dyna = dyna.rename(columns={'user': 'entity_id', 'check_in_time': 'time', 'location_id': 'location'})
+dyna['type'] = 'trajectory'
+# 按照时间的先后顺序排序该 dyna
+dyna = dyna.sort_values(by='time')
+dyna = dyna.reset_index(drop=True)
+dyna['dyna_id'] = dyna.index
+dyna = dyna.reindex(columns=['dyna_id', 'type', 'time', 'entity_id', 'location'])
+dyna.to_csv('./gowalla.dyna', index=False)
+
+
+dyna = pd.read_csv('./gowalla.dyna')
+
+group_location = dyna.groupby('location').count()
+filter_location = group_location[group_location['time'] > 5]
+location_index = filter_location.index.tolist()
+
---
+++ extract_instagram.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from datetime import datetime
+# poi_index = pd.read_csv('./poi_index.txt', sep='\t', names=['index', 'poi'])  # 应该是 index 和 poi name
+word_index = pd.read_csv('./word_index.txt', sep='\t', names=['index', 'word'])
+
+# poi 是 id， text_content 是由 word index 组成的句子，啊这我要给他还原吗，还是给他还原？
+# 还原吧
+train = pd.read_csv('./train.txt', sep='\t', names=['user', 'lat', 'lng', 'timestamp', 'poi', 'text_content'])
+test = pd.read_csv('./test.txt', sep='\t', names=['user', 'lat', 'lng', 'timestamp', 'poi', 'text_content'])
+validation = pd.read_csv('./validation.txt', sep='\t', names=['user', 'lat', 'lng', 'timestamp', 'poi', 'text_content'])
+# 把 train test validation 拼接起来吧
+total_check_in = pd.concat([train, test, validation])
+
+# # 先来做 geo 表
+# poi_info = total_check_in.filter(items=['lat', 'lng', 'poi'])
+# poi_info = poi_info.groupby('location_id').mean()
+# poi_info.reset_index(inplace=True)
+# # 引入 poi name 信息
+# poi = poi_info.merge(poi_index, left_on='poi', right_on='index')
+# poi['type'] = 'Point'
+
+# # 计算 coordinates
+# coordinates = []
+# for index, row in poi.iterrows():
+#     coordinates.append('[{},{}]'.format(row['lng'], row['lat']))
+
+# poi['coordinates'] = coordinates
+# poi = poi.rename(columns={'poi_x': 'geo_id', 'poi_y': 'poi_name'})
+# poi = poi.drop(['lat', 'lng', 'index'])
+# poi = poi.reindex(columns=['geo_id', 'type', 'coordinates', 'poi_name'])
+# poi.to_csv('./instagram/instagram.geo', index=False)
+
+# # 做 usr 表
+# user = pd.unique(total_check_in['user'])
+# user = pd.DataFrame(user, columns=['usr_id'])
+# user.to_csv('./instagram/instagram.usr', index=False)
+
+# 做 dyna 表
+# 需要首先对时间做转换
+time = []
+text = []
+# 居然有 text 为空的，建议删除 760703
+total_check_in = total_check_in.dropna(axis=0, how='any')
+total_check_in = total_check_in[:10]
+for index, row in total_check_in.iterrows():
+    date = datetime.strptime(row['timestamp'], '%Y-%m-%d %H:%M:%S')
+    time.append(date.strftime('%Y-%m-%dT%H:%M:%SZ'))  # 2020-12-07T02:59:46
+    word_index_list = row['text_content'].split(' ')
+    words = ''
+    for w in word_index_list:
+        try:
+            words = words + word_index.iloc[int(w)]['word'] + ' '
+        except TypeError:
+            words += ' '
+    # 最后一个是空格
+    text.append(words[:-1])
+
+total_check_in['time'] = time
+total_check_in['text'] = text
+total_check_in = total_check_in.drop(['lat', 'lng', 'timestamp', 'text_content'], axis=1)
+total_check_in = total_check_in.rename(columns={'poi': 'location', 'user': 'entity_id'})
+total_check_in['type'] = 'traj'
+total_check_in = total_check_in.sort_values(by='time')
+total_check_in = total_check_in.reset_index(drop=True)
+total_check_in['dyna_id'] = total_check_in.index
+total_check_in = total_check_in.reindex(columns=['dyna_id', 'type', 'time', 'entity_id', 'location', 'text'])
+total_check_in.to_csv('./instagram.dyna', index=False)
