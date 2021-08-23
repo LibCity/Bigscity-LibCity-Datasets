c7f2b98dd620e06a6229e01ed7dfb36e4e3fecdc
朱隽凡 <19231055@buaa.edu.cn>
2021-08-23 10:14:59 +0800

    fit input and output path

--- global.py
+++ global.py
@@ -1,11 +1,12 @@
 #link: https://zenodo.org/record/57731#.YSJRbGczZPZ
 import os
 import shutil
+import json
 
-def processGeo(name):
-    geo = open(name + ".geo", "w")
+arcs = nodes = track = geo = dyna = rel = usr = None
+
+def processGeo():
     geo.write("geo_id, type, coordinate\n")
-    nodes = open(name + ".nodes", "r")
     i = 0
     node = nodes.readline()[0 : -1]
     while node != "":
@@ -14,31 +15,22 @@ def processGeo(name):
         geo.write(str(i) + ',Point,"[' + node1 + ',' + node2 + ']"\n')
         i += 1
         node = nodes.readline()[0 : -1]
-    nodes.close()
     nodeNum = i
-
-    nodes = open(name + ".track", "r")
-    node = nodes.readline()[0 : -1]
+    node = track.readline()[0 : -1]
     while node != "":
         node1 = node.split("\t")[0]
         node2 = node.split("\t")[1]
         geo.write(str(i) + ',Point,"[' + node1 + ',' + node2 + ']"\n')
         i += 1
-        node = nodes.readline()[0 : -1]
-    nodes.close()
-    geo.close()
+        node = track.readline()[0 : -1]
     return nodeNum
 
-def processUsr(name):
-    usr = open(name + ".usr", "w")
+def processUsr():
     usr.write("usr_id\n")
     usr.write("0")
-    usr.close()
 
-def processRel(name):
-    rel = open(name + ".rel", "w")
+def processRel():
     rel.write("rel_id,type,origin_id,destination_id\n")
-    arcs = open(name + ".arcs", "r")
     i = 0
     arc = arcs.readline()[0 : -1]
     while arc != "":
@@ -46,57 +38,86 @@ def processRel(name):
         rel.write(str(i) + ',geo,' + arc + '\n')
         i += 1
         arc = arcs.readline()[0: -1]
-    arcs.close()
-    rel.close()
 
-def processDyna(name, nodeNum):
-    dyna = open(name + ".dyna", "w")
+def processDyna(nodeNum):
     dyna.write("dyna_id,type,time,entity_id,location\n")
-    nodes = open(name + ".track", "r")
     i = 0
-    node = nodes.readline()[0: -1]
+    track.seek(0)
+    node = track.readline()[0: -1]
     while node != "":
         dyna.write(str(i) + ',trajectory,' + ',0,' + str(i + nodeNum) + '\n')
         i += 1
-        node = nodes.readline()[0: -1]
+        node = track.readline()[0: -1]
+
+def processConfig():
+    config = dict()
+    config['geo'] = dict()
+    config['geo']['including_types'] = ['Point']
+    config['geo']['Point'] = {}
+    config['usr'] = dict()
+    config['usr']['properties'] = {}
+    config['rel'] = dict()
+    config['rel']['including_types'] = ['geo']
+    config['rel']['geo'] = {}
+    config['dyna'] = dict()
+    config['dyna']['including_types'] = ['trajectory']
+    config['dyna']['trajectory'] = {'entity_id': 'usr_id', 'location': 'geo_id'}
+    config['info'] = dict()
+    config['info']['with_time'] = False
+    json.dump(config, open('config.json', 'w', encoding='utf-8'), ensure_ascii=False)
+
+def openFile(name):
+    global arcs, nodes, track, geo, dyna, rel, usr, config
+    os.chdir("input")
+    os.chdir(name)
+    arcs = open(name + ".arcs", "r")
+    nodes = open(name + ".nodes", "r")
+    track = open(name + ".track", "r")
+    os.chdir(os.path.dirname(os.getcwd()))
+    os.chdir(os.path.dirname(os.getcwd()))
+    outputPath = os.getcwd() + "\\" + "output"
+    outputFilePath = outputPath + "\\" + name
+    if os.path.exists(outputPath):
+        os.chdir(outputPath)
+    else:
+        os.mkdir(outputPath)
+        os.chdir(outputPath)
+    if os.path.exists(outputFilePath):
+        os.chdir(outputFilePath)
+    else:
+        os.mkdir(outputFilePath)
+        os.chdir(outputFilePath)
+    geo = open(name + ".geo", "w")
+    dyna = open(name + ".dyna", "w")
+    rel = open(name + ".rel", "w")
+    usr = open(name + ".usr", "w")
+
+
+def closeFile():
+    global arcs, nodes, track, geo, dyna, rel, usr
+    arcs.close()
     nodes.close()
+    track.close()
+    geo.close()
     dyna.close()
-
-def processConfig(name):
-    # copy config.json file in 00000000
-    if name != "00000000":
-        shutil.copyfile("../00000000/config.json", "config.json")
-
-def delete(name):
-    path = os.getcwd() + "\\" + name + ".arcs"
-    if os.path.exists(path):
-        os.remove(path)
-    path = os.getcwd() + "\\" + name + ".nodes"
-    if os.path.exists(path):
-        os.remove(path)
-    path = os.getcwd() + "\\" + name + ".route"
-    if os.path.exists(path):
-        os.remove(path)
-    path = os.getcwd() + "\\" + name + ".track"
-    if os.path.exists(path):
-        os.remove(path)
+    rel.close()
+    usr.close()
+    os.chdir(os.path.dirname(os.getcwd()))
+    os.chdir(os.path.dirname(os.getcwd()))
 
 def dataTransform(name):
-    processConfig(name)
-    nodeNum = processGeo(name)
-    processUsr(name)
-    processRel(name)
-    processDyna(name, nodeNum)
-    delete(name)
+    openFile(name)
+    nodeNum = processGeo()
+    processUsr()
+    processRel()
+    processDyna(nodeNum)
+    processConfig()
+    closeFile()
 
 if __name__ == '__main__':
-    for i in range(1):
+    for i in range(100):
         if i < 10:
             name = "0000000" + str(i)
         else:
             name = "000000" + str(i)
-        file_dir = os.chdir(name)
         dataTransform(name)
-        os.chdir(os.path.dirname(os.getcwd()))
-
-
--- seattle.py
+++ seattle.py
@@ -1,12 +1,13 @@
 #link: https://www.microsoft.com/en-us/research/publication/hidden-markov-map-matching-noise-sparseness/
 import re
+import os
+import json
+
+network = trackInfo = geo = rel = dyna = usr = None
 
 def processGeoAndRel():
-    geo = open("Seattle.geo", "w")
     geo.write("geo_id, type, coordinate, speed\n")
-    rel = open("Seattle.rel", "w")
     rel.write("rel_id,type,origin_id,destination_id\n")
-    network = open("road_network.txt", "r")
     network.readline()
     line = network.readline()
     nodeInfo = re.search("\(.+\)", line)
@@ -22,20 +23,18 @@ def processGeoAndRel():
             node2 = nodes[i].split(" ")[1]
             geo.write(str(j) + ',Point,"[' + node1 + ',' + node2 + ']"\n')
             if i != len(nodes) - 1:
-                rel.write(str(currentSum) + ',geo,' + str(j) + ',' + str(j + 1) + ',' + speed + '\n')
+                rel.write(str(currentSum) + ',geo,' + str(j) + ',' + str(j + 1) + '\n')
                 currentSum += 1
                 if flag == '1':
-                    rel.write(str(currentSum) + ',geo,' + str(j + 1) + ',' + str(j) + ',' + speed + '\n')
+                    rel.write(str(currentSum) + ',geo,' + str(j + 1) + ',' + str(j) + '\n')
                     currentSum += 1
             i += 1
             j += 1
         line = network.readline()
         nodeInfo = re.search("\(.+\)", line)
-    network.close()
-    rel.close()
     nodeNum = j
 
-    trackInfo = open("gps_data.txt", "r")
+
     trackInfo.readline()
     nodeInfo = re.split('\t| ', trackInfo.readline())
     while len(nodeInfo) == 6:
@@ -44,20 +43,15 @@ def processGeoAndRel():
         geo.write(str(j) + ',Point,"[' + node1 + ',' + node2 + ']"\n')
         j += 1
         nodeInfo = re.split('\t| ', trackInfo.readline())
-    trackInfo.close()
-    geo.close()
     return nodeNum
 
 def processUsr():
-    usr = open("Seattle.usr", "w")
     usr.write("usr_id\n")
     usr.write("0")
-    usr.close()
 
 def processDyna(nodeNum):
-    dyna = open("Seattle.dyna", "w")
     dyna.write("dyna_id,type,time,entity_id,location\n")
-    trackInfo = open("gps_data.txt", "r")
+    trackInfo.seek(0)
     trackInfo.readline()
     nodeInfo = re.split('\t| ', trackInfo.readline())
     i = 0
@@ -67,13 +61,55 @@ def processDyna(nodeNum):
         dyna.write(str(i) + ',trajectory,' + time + ',0,' + str(i + nodeNum) + '\n')
         i += 1
         nodeInfo = re.split('\t| ', trackInfo.readline())
+
+def processConfig():
+    config = dict()
+    config['geo'] = dict()
+    config['geo']['including_types'] = ['Point']
+    config['geo']['Point'] = {}
+    config['usr'] = dict()
+    config['usr']['properties'] = {}
+    config['rel'] = dict()
+    config['rel']['including_types'] = ['geo']
+    config['rel']['geo'] = {'speed': 'num'}
+    config['dyna'] = dict()
+    config['dyna']['including_types'] = ['trajectory']
+    config['dyna']['trajectory'] = {'entity_id': 'usr_id', 'location': 'geo_id'}
+    config['info'] = dict()
+    json.dump(config, open('config.json', 'w', encoding='utf-8'), ensure_ascii=False)
+
+def openFile():
+    global network, trackInfo, geo, rel, dyna, usr
+    os.chdir("input")
+    network = open("road_network.txt", "r")
+    trackInfo = open("gps_data.txt", "r")
+    os.chdir(os.path.dirname(os.getcwd()))
+    outputPath = os.getcwd() + "\\" + "output"
+    if os.path.exists(outputPath):
+        os.chdir(outputPath)
+    else:
+        os.mkdir(outputPath)
+    geo = open("Seattle.geo", "w")
+    rel = open("Seattle.rel", "w")
+    dyna = open("Seattle.dyna", "w")
+    usr = open("Seattle.usr", "w")
+
+def closeFile():
+    global network, trackInfo, geo, rel, dyna, usr
+    network.close()
     trackInfo.close()
+    geo.close()
+    rel.close()
     dyna.close()
+    usr.close()
 
 def dataTransform():
+    openFile()
     nodeNum = processGeoAndRel()
     processUsr()
     processDyna(nodeNum)
+    processConfig()
+    closeFile()
 
 if __name__ == '__main__':
     dataTransform()
