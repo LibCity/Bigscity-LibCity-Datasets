7e7f25eda538f54ad253117fafc0841c9c1e9771
朱隽凡 <19231055@buaa.edu.cn>
2021-09-05 15:29:30 +0800

    add .route part of global and seattle dataset

--- global.py
+++ global.py
@@ -1,43 +1,46 @@
 #link: https://zenodo.org/record/57731#.YSJRbGczZPZ
 import os
-import shutil
 import json
 
-arcs = nodes = track = geo = dyna = rel = usr = None
+arcs = nodes = track = geo = dyna = rel = usr = route_unprocessed = route_processed = None
+
 
 def processGeo():
     geo.write("geo_id, type, coordinate\n")
     i = 0
-    node = nodes.readline()[0 : -1]
+    node = nodes.readline()[0:-1]
     while node != "":
         node1 = node.split("\t")[0]
         node2 = node.split("\t")[1]
         geo.write(str(i) + ',Point,"[' + node1 + ',' + node2 + ']"\n')
         i += 1
-        node = nodes.readline()[0 : -1]
+        node = nodes.readline()[0:-1]
     nodeNum = i
-    node = track.readline()[0 : -1]
+    node = track.readline()[0:-1]
     while node != "":
         node1 = node.split("\t")[0]
         node2 = node.split("\t")[1]
         geo.write(str(i) + ',Point,"[' + node1 + ',' + node2 + ']"\n')
         i += 1
-        node = track.readline()[0 : -1]
+        node = track.readline()[0:-1]
     return nodeNum
+
 
 def processUsr():
     usr.write("usr_id\n")
     usr.write("0")
 
+
 def processRel():
     rel.write("rel_id,type,origin_id,destination_id\n")
     i = 0
-    arc = arcs.readline()[0 : -1]
+    arc = arcs.readline()[0:-1]
     while arc != "":
         arc = arc.replace('\t', ',')
         rel.write(str(i) + ',geo,' + arc + '\n')
         i += 1
         arc = arcs.readline()[0: -1]
+
 
 def processDyna(nodeNum):
     dyna.write("dyna_id,type,time,entity_id,location\n")
@@ -48,6 +51,15 @@ def processDyna(nodeNum):
         dyna.write(str(i) + ',trajectory,' + ',0,' + str(i + nodeNum) + '\n')
         i += 1
         node = track.readline()[0: -1]
+
+
+def processRoute():
+    route_processed.write("rel_id\n")
+    line = route_unprocessed.readline()
+    while line != "":
+        route_processed.write(line)
+        line = route_unprocessed.readline()
+
 
 def processConfig():
     config = dict()
@@ -66,13 +78,15 @@ def processConfig():
     config['info']['with_time'] = False
     json.dump(config, open('config.json', 'w', encoding='utf-8'), ensure_ascii=False)
 
+
 def openFile(name):
-    global arcs, nodes, track, geo, dyna, rel, usr, config
+    global arcs, nodes, track, geo, dyna, rel, usr, config, route_unprocessed, route_processed
     os.chdir("input")
     os.chdir(name)
     arcs = open(name + ".arcs", "r")
     nodes = open(name + ".nodes", "r")
     track = open(name + ".track", "r")
+    route_unprocessed = open(name + ".route", "r")
     os.chdir(os.path.dirname(os.getcwd()))
     os.chdir(os.path.dirname(os.getcwd()))
     outputPath = os.getcwd() + "\\" + "output"
@@ -91,10 +105,11 @@ def openFile(name):
     dyna = open(name + ".dyna", "w")
     rel = open(name + ".rel", "w")
     usr = open(name + ".usr", "w")
+    route_processed = open(name + ".route", "w")
 
 
 def closeFile():
-    global arcs, nodes, track, geo, dyna, rel, usr
+    global arcs, nodes, track, geo, dyna, rel, usr, route_unprocessed, route_processed
     arcs.close()
     nodes.close()
     track.close()
@@ -102,8 +117,11 @@ def closeFile():
     dyna.close()
     rel.close()
     usr.close()
+    route_unprocessed.close()
+    route_processed.close()
     os.chdir(os.path.dirname(os.getcwd()))
     os.chdir(os.path.dirname(os.getcwd()))
+
 
 def dataTransform(name):
     openFile(name)
@@ -111,8 +129,10 @@ def dataTransform(name):
     processUsr()
     processRel()
     processDyna(nodeNum)
+    processRoute()
     processConfig()
     closeFile()
+
 
 if __name__ == '__main__':
     for i in range(100):
